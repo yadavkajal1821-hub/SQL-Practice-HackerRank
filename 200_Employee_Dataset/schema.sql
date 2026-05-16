c6cb740d699fc2bc1d39189d235023cc70450eea
kajal yadav <yadavkajal1821@gmail.com>
2026-05-16 11:01:05 -0400

    Update schema.sql

--- 200_Employee_Dataset/schema.sql
+++ 200_Employee_Dataset/schema.sql
@@ -1,1 +1,10 @@
+-- Problem: [Problem name here]
+-- Platform: HackerRank / LeetCode
+-- Difficulty: Easy / Medium / Hard
+-- Topic: Basic SELECT / Aggregation / etc.
 
+-- My Solution:
+SELECT * FROM employees;
+
+-- What I learned:
+-- [write one line about what this query taught you]
